6dfc5c3d31a36538a2c0e50c9ddedf84e66aba1b
Scott Bassett <S.Bassett@ucas.ac.uk>
2020-04-03 15:21:05 +0100

    working on authorisation

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace DutchTreat
 
             using var scope = scopeFactory.CreateScope();
             var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
-            seeder.Seed();
+            seeder.SeedAsync().Wait();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
